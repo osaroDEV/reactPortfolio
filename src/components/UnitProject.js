d02ab29b8d95fb3386a58a29bc636341dd220932
Osaro Onaiwu <Osernon@gmail.com>
2021-07-14 15:30:47 +0100

    add link arrows to project cards

--- src/components/UnitProject.js
+++ src/components/UnitProject.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BsArrowRight } from 'react-icons/bs';
 
 const Container = styled.div`
   height: 320px;
@@ -18,19 +19,43 @@ const Container = styled.div`
     box-shadow: rgb(34 15 108 / 9%) 0px 28px 30px 8px;
   }
 `;
+
+const MoreIcon = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  height: 30px;
+  width: 40px;
+  background: blue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const UnitProject = ({ id, title, src, url }) => {
   return (
-    
-    <a style={{textDecoration: 'none', color: 'inherit'}} href={url} target='_blank' rel='noreferrer'><Container>
-        <p style={{ textAlign: 'left', fontWeight: 900, fontSize: '1.1rem', textShadow: '2px 2px 2px #ecd672' }}>
+    <a
+      style={{ textDecoration: 'none', color: 'inherit' }}
+      href={url}
+      target='_blank'
+      rel='noreferrer'
+    >
+      <Container>
+        <p
+          style={{
+            textAlign: 'left',
+            fontWeight: 900,
+            fontSize: '1.1rem',
+            textShadow: '2px 2px 2px #ecd672',
+          }}
+        >
           {title}
         </p>
         <img
           style={{
             display: 'block',
             // width: 'calc(100% - 3.6rem)',
-            width: '260px',
-            height: '126.88px',
+            width: '260.000px',
+            height: '146.178px',
             position: 'absolute',
             top: '35%',
             left: '50%',
@@ -39,8 +64,11 @@ const UnitProject = ({ id, title, src, url }) => {
           src={src}
           alt={title}
         />
-      </Container></a>
-    
+        <MoreIcon>
+          <BsArrowRight style={{ fontSize: '2.5em', color: '#fff' }} />
+        </MoreIcon>
+      </Container>
+    </a>
   );
 };
 
