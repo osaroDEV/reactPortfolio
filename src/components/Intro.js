f860973f86882ef33963c661ff1b70076a067873
osarodev <osernon@gmail.com>
2023-10-26 18:00:38 +0100

    refactor links

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -56,7 +56,7 @@ const Intro = () => {
         </motion.p>
         <a
           className='proj'
-          href='https://drive.google.com/file/d/1PFFK2zgKze-Vep_UkxdWC6ik5J_I_eqV/view?usp=sharing'
+          href='https://drive.google.com/file/d/1sPazUISF1Lnl2o_aFm4qpN5wsjdZoIO7/view?usp=sharing'
           rel='noreferrer'
           target='_blank'
         >
