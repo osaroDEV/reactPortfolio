fd83e93b36da7d5c807bef0e52a0f04db094561b
osarodev <osernon@gmail.com>
2024-02-23 18:21:52 +0100

    update resume

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -56,7 +56,7 @@ const Intro = () => {
         </motion.p>
         <a
           className='proj'
-          href='https://drive.google.com/file/d/1tGOmrWReRvHM2LnLFYH7YAXu12e41VFO/view?usp=drive_link'
+          href='https://drive.google.com/file/d/1PFFK2zgKze-Vep_UkxdWC6ik5J_I_eqV/view?usp=drive_link'
           rel='noreferrer'
           target='_blank'
         >
